e45ef07e8ec3abac9669205e38c9073846e432c6
Randy Tate <github@tateventures.com>
2024-01-20 23:53:24 -0700

    Updates to package lists.

---
+++ uganda/2024/mwebaza/install-packages.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+PACKAGE_FILE=install-packages.txt
+if [ $# -gt 0 ] ; then
+	if [ -f $1 ] ; then
+		PACKAGE_FILE=$1
+	fi
+fi
+
+echo "================================="
+echo "= Installing packages from:"
+echo "=  $PACKAGE_FILE"
+echo "================================="
+read -p "Proceed? <y/n> " prompt
+if [[ $prompt =~ [yY](es)* ]]
+then
+	echo "installing ..."
+else
+	exit
+fi
+
+cat $PACKAGE_FILE | grep -v -e '^$' -e '^#' | xargs sudo apt-get install -y
+
+
+##################################
+# Remove packages
+##################################
+
+REMOVE_PACKAGE_FILE=remove-packages.txt
+if [ $# -gt 1 ] ; then
+	if [ -f $2 ] ; then
+		REMOVE_PACKAGE_FILE=$2
+	fi
+fi
+
+echo "================================="
+echo "= Removing  packages from:"
+echo "=  $REMOVE_PACKAGE_FILE"
+echo "================================="
+read -p "Proceed? <y/n> " prompt
+if [[ $prompt =~ [yY](es)* ]]
+then
+	echo "removing ..."
+else
+	exit
+fi
+
+cat $REMOVE_PACKAGE_FILE | grep -v -e '^$' -e '^#' | xargs sudo apt-get remove -y
+
+sudo apt autoremove -y
+
