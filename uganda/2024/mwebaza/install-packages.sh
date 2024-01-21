d628a52f28ac435cf474ac24ec3b4c4e4cea3295
Randy Tate <github@tateventures.com>
2024-01-21 12:11:00 -0700

    Updates to package lists.

--- uganda/2024/mwebaza/install-packages.sh
+++ uganda/2024/mwebaza/install-packages.sh
@@ -7,9 +7,20 @@ if [ $# -gt 0 ] ; then
 	fi
 fi
 
+REMOVE_PACKAGE_FILE=remove-packages.txt
+if [ $# -gt 1 ] ; then
+	if [ -f $2 ] ; then
+		REMOVE_PACKAGE_FILE=$2
+	fi
+fi
+
+
 echo "================================="
 echo "= Installing packages from:"
 echo "=  $PACKAGE_FILE"
+echo "="
+echo "= Removing packages from:"
+echo "=  $REMOVE_PACKAGE_FILE"
 echo "================================="
 read -p "Proceed? <y/n> " prompt
 if [[ $prompt =~ [yY](es)* ]]
@@ -21,29 +32,12 @@ fi
 
 cat $PACKAGE_FILE | grep -v -e '^$' -e '^#' | xargs sudo apt-get install -y
 
-
-##################################
-# Remove packages
-##################################
-
-REMOVE_PACKAGE_FILE=remove-packages.txt
-if [ $# -gt 1 ] ; then
-	if [ -f $2 ] ; then
-		REMOVE_PACKAGE_FILE=$2
-	fi
-fi
-
 echo "================================="
-echo "= Removing  packages from:"
-echo "=  $REMOVE_PACKAGE_FILE"
+echo "= Installs completed."
+echo "="
+echo "= Proceeding with package"
+echo "= removal ..."
 echo "================================="
-read -p "Proceed? <y/n> " prompt
-if [[ $prompt =~ [yY](es)* ]]
-then
-	echo "removing ..."
-else
-	exit
-fi
 
 cat $REMOVE_PACKAGE_FILE | grep -v -e '^$' -e '^#' | xargs sudo apt-get remove -y
 
